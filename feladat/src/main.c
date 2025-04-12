ac4187349e166e4534efd33f7bf27d806e704e74
Rferencsik <robert.ferencsik@student.uni-miskolc.hu>
2025-04-12 13:23:24 +0200

    Modify: make and main, app is running in window

--- feladat/src/main.c
+++ feladat/src/main.c
@@ -1,3 +1,5 @@
+#include "app.h"
+
 #include <SDL2/SDL.h>
 
 #include <stdio.h>
@@ -8,5 +10,15 @@
 int main(int argc, char* argv[])
 {
 	printf("Game is running...\n");
+       App app;
+
+    init_app(&app, 800, 600);
+    while (app.is_running) {
+        handle_app_events(&app);
+        update_app(&app);
+        render_app(&app);
+    }
+    destroy_app(&app);
+
     return 0;
 }
